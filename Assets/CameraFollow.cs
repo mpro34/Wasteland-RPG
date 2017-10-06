87dcf861d875bf0c0044748868a7c81450603127
Chris Whiting <mpro34@gmail.com>
2017-10-05 19:07:01 -0700

    First commit

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	GameObject player;
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag ("Player");
+		print (player.ToString ());
+	}
+	
+	// LateUpdate is called after player position is calculated
+	void LateUpdate () {
+		transform.position = player.transform.position;
+	}
+}
