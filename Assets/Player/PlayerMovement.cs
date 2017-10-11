b74b83a071e0f1b7d2bdf3108fd73bf68cc8d8f2
Chris Whiting <mpro34@gmail.com>
2017-10-10 20:57:28 -0700

    Added movement gizmos, healthbar

--- Assets/Player/PlayerMovement.cs
+++ Assets/Player/PlayerMovement.cs
@@ -7,9 +7,11 @@ public class PlayerMovement : MonoBehaviour
 {
 
 	[SerializeField] float walkMoveStopRadius = 0.2f;
+    [SerializeField] float attackMoveStopRadius = 5.0f;
     ThirdPersonCharacter thirdPersonCharacter;   // A reference to the ThirdPersonCharacter on the object
     CameraRaycaster cameraRaycaster;
-    Vector3 currentClickTarget;
+    Vector3 currentDestination, clickPoint;
+
 
 	bool isInDirectMode = false; 
         
@@ -17,7 +19,7 @@ public class PlayerMovement : MonoBehaviour
     {
         cameraRaycaster = Camera.main.GetComponent<CameraRaycaster>();
         thirdPersonCharacter = GetComponent<ThirdPersonCharacter>();
-        currentClickTarget = transform.position;
+        currentDestination = transform.position;
     }
 
     // Fixed update is called in sync with physics
@@ -26,7 +28,7 @@ public class PlayerMovement : MonoBehaviour
 		if (Input.GetKeyDown(KeyCode.G)) //G for gamepad. TODO add to menu
 		{
 			isInDirectMode = !isInDirectMode; // toggle mode
-            currentClickTarget = transform.position; //Clear click target
+            currentDestination = transform.position; //Clear click target
         }
 
 		if (isInDirectMode)
@@ -53,31 +55,53 @@ public class PlayerMovement : MonoBehaviour
 	}
 
 	void ProcessMouseMovement ()
-	{
-		if (Input.GetMouseButton (0)) {
-			print ("Cursor raycast hit = " + cameraRaycaster.hit.collider.gameObject.name.ToString ());
-			switch (cameraRaycaster.currentLayerHit) {
-			case Layer.Walkable:
-				//Only move on Walkable layer
-				currentClickTarget = cameraRaycaster.hit.point;
-				// So not set in default case
-				break;
-			case Layer.Enemy:
-				print ("Not moving to enemy");
-				break;
-			default:
-				print ("Unexpected layer found");
-				return;
-			}
-		}
-		var playerToClickPoint = currentClickTarget - transform.position;
-		if (playerToClickPoint.magnitude >= walkMoveStopRadius) {
-			thirdPersonCharacter.Move (playerToClickPoint, false, false);
-		} 
-		else 
-		{
-			thirdPersonCharacter.Move (Vector3.zero, false, false);
-		}
-	}
+    {
+        if (Input.GetMouseButton(0))
+        {
+            clickPoint = cameraRaycaster.hit.point;
+            switch (cameraRaycaster.currentLayerHit)
+            {
+                case Layer.Walkable:
+                    currentDestination = ShortDestination(clickPoint, walkMoveStopRadius);
+                    break;
+                case Layer.Enemy:
+                    currentDestination = ShortDestination(clickPoint, attackMoveStopRadius);
+                    break;
+                default:
+                    print("Unexpected layer found");
+                    return;
+            }
+        }
+
+        WalkToDestination();
+    }
+
+    private void WalkToDestination()
+    {
+        var playerToClickPoint = currentDestination - transform.position;
+        if (playerToClickPoint.magnitude >= walkMoveStopRadius)
+        {
+            thirdPersonCharacter.Move(playerToClickPoint, false, false);
+        }
+        else
+        {
+            thirdPersonCharacter.Move(Vector3.zero, false, false);
+        }
+    }
+
+    Vector3 ShortDestination(Vector3 destination, float shortening)
+    {
+        Vector3 reductionVector = (destination - transform.position).normalized * shortening;
+        return destination - reductionVector;
+    }
+
+    void OnDrawGizmos()
+    {
+        //draw mocement gizmos
+        Gizmos.color = Color.black;
+        Gizmos.DrawLine(transform.position, currentDestination);
+        Gizmos.DrawSphere(currentDestination, 0.1f);
+        Gizmos.DrawSphere(clickPoint, 0.15f);
+    }
 }
 
